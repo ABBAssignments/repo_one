45003232821017d9f837e00e7315e02e745c4908
Siva Shankar.S <allindiangeek@gmail.com>
2024-10-11 07:25:02 +0530

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -1,9 +1,21 @@
 # outputs.tf
 
 output "aks_cluster_name" {
-  value = azurerm_kubernetes_cluster.aks_cluster.name
+  description = "The name of the AKS cluster"
+  value       = azurerm_kubernetes_cluster.aks_cluster.name
+}
+
+output "log_analytics_workspace_id" {
+  description = "The ID of the Log Analytics workspace"
+  value       = azurerm_log_analytics_workspace.log_analytics.id
+}
+
+output "aks_resource_group" {
+  description = "The name of the resource group for AKS"
+  value       = azurerm_resource_group.aks_rg.name
 }
 
 output "aks_cluster_id" {
-  value = azurerm_kubernetes_cluster.aks_cluster.id
+  description = "The ID of the AKS cluster"
+  value       = azurerm_kubernetes_cluster.aks_cluster.id
 }
