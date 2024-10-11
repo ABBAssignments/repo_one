90ec9a8a1bc26b0bda598ba45cb6b0f207ca737e
Siva Shankar.S <allindiangeek@gmail.com>
2024-10-11 07:24:26 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -11,6 +11,22 @@ resource "azurerm_resource_group" "aks_rg" {
   location = var.location
 }
 
+# Define a virtual network
+resource "azurerm_virtual_network" "aks_vnet" {
+  name                = "${var.resource_group_name}-vnet"
+  address_space       = ["10.0.0.0/16"]
+  location            = var.location
+  resource_group_name = azurerm_resource_group.aks_rg.name
+}
+
+# Define a subnet for AKS
+resource "azurerm_subnet" "aks_subnet" {
+  name                 = "${var.resource_group_name}-subnet"
+  resource_group_name  = azurerm_resource_group.aks_rg.name
+  virtual_network_name = azurerm_virtual_network.aks_vnet.name
+  address_prefixes     = ["10.0.1.0/24"]
+}
+
 # Define Azure Kubernetes Service (AKS)
 resource "azurerm_kubernetes_cluster" "aks_cluster" {
   name                = var.aks_cluster_name
@@ -22,6 +38,7 @@ resource "azurerm_kubernetes_cluster" "aks_cluster" {
     name       = "default"
     node_count = 1
     vm_size    = "Standard_DS2_v2"
+    vnet_subnet_id = azurerm_subnet.aks_subnet.id
   }
 
   identity {
@@ -43,9 +60,9 @@ resource "azurerm_log_analytics_workspace" "log_analytics" {
 }
 
 resource "azurerm_monitor_diagnostic_setting" "aks_monitoring" {
-  name               = "aksmonitor"
-  target_resource_id = azurerm_kubernetes_cluster.aks_cluster.id
-  log_analytics_workspace_id = azurerm_log_analytics_workspace.log_analytics.id
+  name                         = "aksmonitor"
+  target_resource_id           = azurerm_kubernetes_cluster.aks_cluster.id
+  log_analytics_workspace_id   = azurerm_log_analytics_workspace.log_analytics.id
 
   logs {
     category = "kube-apiserver"
