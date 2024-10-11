a49ffd94416af05dcc45b23260d02fac2cfa86ef
Siva Shankar.S <allindiangeek@gmail.com>
2024-10-11 07:24:48 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,18 +1,17 @@
 # variables.tf
 
 variable "resource_group_name" {
-  type        = string
   description = "The name of the resource group"
+  type        = string
 }
 
 variable "location" {
+  description = "The Azure region to deploy resources"
   type        = string
-  description = "Azure region where the resources will be created"
   default     = "East US"
 }
 
 variable "aks_cluster_name" {
-  type        = string
   description = "The name of the AKS cluster"
-  default     = "myAKSCluster"
+  type        = string
 }
